dbdd44e21eb3be42ec13c9765cca17c4215eeafd
Mesut Butun <mstbtn@gmail.com>
2020-02-29 12:53:34 -0800

    update

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -5,6 +5,7 @@ public class LoopPractice {
         for (int i = 0; i < 10; i++) {
             System.out.println("Counter :"+i);
             System.out.println("################");
+            System.out.println("I changed something");
 
         }
     }
